3d287ba692c308eec0d165a2067f320429664641
Tim Dijkhuizen <tim.dijkhuizen@os3.nl>
2018-03-15 14:43:09 +0000

    Adbpull script and some small changes

--- 3export_script.sh
+++ 3export_script.sh
@@ -4,21 +4,25 @@
 
 #adb shell ls -a | while read line
 pull_recur(){
-	for line in $(adb shell ls -a $1)
+	for line in $(adb shell "$1"/*)
 	do
-		line="${line//[^a-zA-Z0-9_.-]/}"
-		if [ "$line" = "etc" ]
+#		line="${line//[^a-zA-Z0-9_.-]/}"
+#		if [ "$line" = "etc" ]
+#		then
+#			echo "Skipped"
+#		else
+	if [ -l "$line" ]
+	then
+		echo "Skipped $line"
+	else
+	#	line="$1"/"$line"
+		echo "$line"
+		adb pull -a $line /mnt/isaacscript/pull_latest2/"$line"
+		if [ ! $? -eq 0 ]
 		then
-			echo "Skipped"
-		else
-			line="$1"/"$line"
-			echo "$line"
-			adb pull -a $line /mnt/isaacscript/pull_latest2/"$line"
-			if [ ! $? -eq 0 ]
-			then
-				echo "Niet gelukt: $line"
-			fi
+			echo "Niet gelukt: $line"
 		fi
+	fi
 	done
 }
 pull_recur $1
---
+++ adbpull.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#If no arguments are given show usage
+usage="adbpull.sh is a program that automates a pull of data on a Android device.\n
+In order to use the program an argument has to be given where the backup (locally) will be stored...\n\n
+e.g. ./adbpull.sh REMOTE LOCAL"
+
+if [[ $# -eq 0 ]] ; then
+	echo -e $usage
+	exit 0
+fi
+
+#Declare variables from arguments
+remote_location=$1
+local_location=$2
+device_name=$(adb devices | tail -2 | head -1 | awk '{print $1}')
+device_state=$(adb devices | tail -2 | head -1 | awk '{print $2}')
+
+#Check device state
+if [[ $device_state = "unauthorized" ]]; then
+	echo "Device '$device_name' is unauthorized, please authorize on device and start script again."
+	exit
+fi
+
+#Start ADB server
+adb start-server
+echo "ADB server started"
+echo "Performing a backup of device $device_name saved on '$local_location'"
+
+mkdir $local_location
+#adb shell ls -a | while read line
+pull_recur(){
+	pull_object=$1
+	# Remove backslash if already there
+
+	for line in $(adb shell ls -a $pull_object)
+	do
+                if [[ $pull_object = "/" ]]; then
+                        pull_object=""
+                fi
+		# Sanitize ls output
+		line="${line//[^a-zA-Z0-9_.-]/}"
+		# Skip /proc directory to prevent timeout errors
+		if [ "$line" = "d" ] || [ "$line" = "proc" ] || [ "$line" = "sys" ]
+		then
+			echo "Skipped $line"
+		else
+			line="$pull_object"/"$line"
+			echo "Trying to pull: $line"
+			adb pull -a $line $local_location"$line"
+			if [ ! $? -eq 0 ]
+			then
+				echo "Fail: $line"
+			else
+				hashFolder=$(tar c $local_location"$line" | sha256sum)
+				echo "$hashFolder $local_location$line" | tee -a $local_location/hash.txt
+			fi
+		fi
+	done
+}
+pull_recur $remote_location
