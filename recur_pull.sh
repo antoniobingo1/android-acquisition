e7bde9006bcb27fdf5edd1d925439096b5320eb3
Tim Dijkhuizen <tim.dijkhuizen@os3.nl>
2018-03-22 19:43:47 +0000

    Fix a Permission denied catch and added an echo about doing the hash on files

--- recur_pull.sh
+++ recur_pull.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #If no arguments are given show usage
-usage="adbpull.sh is a program that automates a pull of data on a Android device.\n
+usage="adbpull.sh is a program that automates a (recursive) pull of data on a Android device.\n
 In order to use the program an argument has to be given where the backup (locally) will be stored...\n\n
 e.g. ./adbpull.sh REMOTE LOCAL"
 
@@ -31,9 +31,12 @@ mkdir $local_location
 pull_recur(){
 	local pull_object=$1
 	echo "Pull object is: $pull_object"
-	if [ "$pull_object" == "//acct" ] || [ "$pull_object" == "//mnt/shell/emulated/0/Android" ]; then
-		return;
-	fi;
+
+	# Check if folder is /proc or /sys and if so skip folder
+	if [ "$pull_object" == "//proc" ] || [ "$pull_object" == "//sys" ]
+	then
+		return
+	fi
 
 	# Pull files
 	local list_of_files=$(adb shell "ls -la '$pull_object' | grep '^-' | grep -Eo '[^ ]+$'")
@@ -42,29 +45,29 @@ pull_recur(){
 	then
 	        echo "Failure on list for files: No such file or directory"
         else
-        	echo "$list_of_files" | grep 'opendir failed' &> /dev/null
+        	echo "$list_of_files" | grep -i 'permission denied' &> /dev/null
 		if [ ! $? == 0 ]
 		then
-			echo "Our pretty list of files is $list_of_files"
+			echo -e "List of files:\n$list_of_files\n"
 			for file in $list_of_files
 			do
 				local file="$(echo -n "$file" | tr -d '\r')"
-				if [ "$pull_object/$file" == "//proc/mlog" ] || [ "$pull_object/$file" == "//proc/ccci_log" ] || [ "$pull_object/$file" == "//proc/sysram" ] || [ "$pull_object/$file" == "//proc/zraminfo" ] || [ "$pull_object/$file" == "//proc/sysram_flag" ]
-				then
-					continue;
-				fi
+				#if [ "$pull_object/$file" == "FILE" ]
+				#then
+				#	continue
+				#fi
 				echo "Trying to pull: $pull_object"/"$file"
 				adb pull -a "$pull_object"/"$file" "$local_location"/"$pull_object"/
 				if [ ! $? -eq 0 ]
 				then
-					echo "Fail: $pull_object/$file"
+					echo "Failure on: $pull_object/$file"
 				fi
 			done
 		else
-			echo "Failure on list for files: opendir failed, Permission denied"
+			echo "Failure on list for files: Permission denied"
 		fi
 	fi
-	echo -e "\n\nPulled all files in directory: $pull_object \n\n"
+	echo -e "\n\nPulled all files in directory: $pull_object\n\n"
 
 	# Recurse through directories
 	local list_of_dirs=$(adb shell "ls -la '$pull_object' | grep '^d' | grep -Eo '[^ ]+$'")
@@ -73,29 +76,32 @@ pull_recur(){
 	then
 		echo "Failure on list for directories: No such file or directory"
 	else
-		echo $list_of_dirs | grep 'opendir failed' &> /dev/null
+		echo $list_of_dirs | grep -i 'permission denied' &> /dev/null
 		if [ ! $? == 0 ]
 		then
+			# Check if a empty list of directory is returned
 			if [ ! -z "$list_of_dirs" ]
 			then
+				echo -e "List of directories:\n$list_of_dirs\n"
 				for directory in $list_of_dirs
 				do
 					local directory="$(echo -n "$directory" | tr -d '\r')"
-					echo "List of dirs = $list_of_dirs"
 					mkdir -p "$local_location"/"$pull_object"/"$directory"
 					echo "Pull_object = $pull_object and dir = $directory"
 					echo -e "\n\n Calling function pull_recur $pull_object/$directory \n\n"
 					pull_recur "$pull_object/$directory"
 				done
 			else
-				echo "No directories found"
+				echo "Failure on $pull_object: No directories found"
 			fi
 		else
-			echo "Failure on list for directories: opendir, Permission denied"
+			echo "Failure on list for directories: Permission denied"
 		fi
 	fi
 }
 pull_recur $remote_location
 
 #Make a hash of all pulled files
+echo "Done pulling all files from , now creating a hash of all files... This could take some time."
 find $local_location -type f | while read files; do echo $(sha256sum "$files"); done > $local_location/hash_files.txt
+echo "A hash of all the files can be found in '$local_location/hash_files.txt'"
